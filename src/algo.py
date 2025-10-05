fe265c45560d74710eb869b9fe382c4efee3fb7a
joshuaeng <117681341+joshuaeng@users.noreply.github.com>
2025-10-05 12:03:55 +0200

    First Commit

--- src/algo.py
+++ src/algo.py
@@ -19,7 +19,7 @@ class Algo(ABC):
         """
         self.alpaca_market_data = AlpacaMarketData()
         self.alpaca_trading = AlpacaTrading()
-        self._cache = cache
+        self.cache = cache
 
     @abstractmethod
     async def run(self):
@@ -28,52 +28,3 @@ class Algo(ABC):
         the child classes.
         """
         pass
-
-    def _get_from_cache(self, key: str):
-        """
-        Get a value from the cache.
-
-        Args:
-            key (str): The key of the value to get.
-
-        Returns:
-            The value from the cache, or None if the key is not found.
-        """
-        return self._cache.get(key)
-
-    def _put_in_cache(self, key: str, value):
-        """
-        Put a value in the cache.
-
-        Args:
-            key (str): The key of the value to put.
-            value: The value to put in the cache.
-        """
-        self._cache.put(key, value)
-
-    def _has_in_cache(self, key: str) -> bool:
-        """
-        Check if a key is in the cache.
-
-        Args:
-            key (str): The key to check.
-
-        Returns:
-            bool: True if the key is in the cache, False otherwise.
-        """
-        return self._cache.has(key)
-
-    def _remove_from_cache(self, key: str):
-        """
-        Remove a value from the cache.
-
-        Args:
-            key (str): The key of the value to remove.
-        """
-        self._cache.remove(key)
-
-    def _clear_cache(self):
-        """
-        Clear the entire cache.
-        """
-        self._cache.clear()
